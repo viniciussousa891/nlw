a38bf6c3ac13ff187a07340e581f48e2a952a859
viniciussousa891 <sousav387@gmail.com>
2020-06-04 12:17:54 -0300

    nodejs config

--- src/server.js
+++ src/server.js
@@ -25,6 +25,10 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
 
 // ligar o servidor
 server.listen(3000)

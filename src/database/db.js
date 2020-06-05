c9af5f69c33c4fa3e84cadf94682ffb0f4c8432c
viniciussousa891 <sousav387@gmail.com>
2020-06-05 14:10:29 -0300

    db.js

--- src/database/db.js
+++ src/database/db.js
@@ -4,8 +4,10 @@ const sqlite3 = require("sqlite3").verbose()
 // criar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db")
 
+module.exports = db
+
 // utilizar o objeto de banco de dados para nossas operações
-db.serialize(() => {
+/*  db.serialize(() => {
     // com comandos SQL eu vou:
 
     // 1 criar uma tabela com comandos SQL 
@@ -37,13 +39,13 @@ db.serialize(() => {
     `
 
     const values = [
-        "https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-1.2.1&auto=format&fit=crop&w=1174&q=80",
-        "Colectoria",
+        "https://images.unsplash.com/photo-1528190336454-13cd56b45b5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "Papersider",
         "Guilherme Gemballa, Jardim América",
         "Número 260",
         "Santa Catarina",
         "Rio do Sul",
-        "Resíduos Eletrônicos, Lâmpadas"
+        "Papéis e Papelão"
     ]
 
     function afterInsertData(err) {
@@ -54,19 +56,25 @@ db.serialize(() => {
         console.log(this)
     }
 
-//db.run(query, values, afterInsertData)
+db.run(query, values, afterInsertData) */ 
 
     // 3 consultar dados na tabela
-    db.all(`SELECT * FROM places`, function(err, rows) {
-        if(err) {
-            return console.log(err)
-    }
+//    db.all(`SELECT name FROM places`, function(err, rows) {
+//        if(err) {
+//            return console.log(err)
+//    }
 
-    console.log("Aqui estão seus registros")
-    console.log(rows)
-    })
+//    console.log("Aqui estão seus registros")
+//    console.log(rows)
+//    })
 
 
-    // 4 Deletar dado da tabela
+        // 4 Deletar dado da tabela
+    //  db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+    //      if(err) {
+    //          return console.log(err)
+    //      }
 
-})
+    //      console.log("Registro deletado com sucesso!")
+    //  })
+//})
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+/* pegar o banco de dados */
+const db = require("./database/db")
+
 
 // configurar pasta publica
 server.use(express.static("public"))
@@ -26,6 +29,19 @@ server.get("/create-point", (req, res) => {
 })
 
 server.get("/search", (req, res) => {
+
+
+/* Pegar os dados do banco de dados */
+    db.all(`SELECT * FROM places`, function(err, rows) {
+                if(err) {
+                    return console.log(err)
+            }
+        
+            console.log("Aqui estão seus registros")
+            console.log(rows)
+            })
+
+
     return res.render("search-results.html")
 })
 

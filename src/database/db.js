dbb44e52f050baf1b93cfe97ecdba7fb453e54f0
viniciussousa891 <sousav387@gmail.com>
2020-06-05 21:16:28 -0300

    update search results

--- src/database/db.js
+++ src/database/db.js
@@ -22,10 +22,10 @@ module.exports = db
             city TEXT,
             items TEXT
         );
-    `)
+    `) */
 
     // 2 Inserir dados na tabela
-    const query = `
+  /*   const query = `
         INSERT INTO places (
             image,
             name,
@@ -56,25 +56,24 @@ module.exports = db
         console.log(this)
     }
 
-db.run(query, values, afterInsertData) */ 
+db.run(query, values, afterInsertData)  */
 
     // 3 consultar dados na tabela
-//    db.all(`SELECT name FROM places`, function(err, rows) {
-//        if(err) {
-//            return console.log(err)
-//    }
+  /*  db.all(`SELECT name FROM places`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+    }
 
-//    console.log("Aqui estão seus registros")
-//    console.log(rows)
-//    })
-
+    console.log("Aqui estão seus registros")
+    console.log(rows)
+   })
+*/
 
         // 4 Deletar dado da tabela
-    //  db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
-    //      if(err) {
-    //          return console.log(err)
-    //      }
+     /*  db.run(`DELETE FROM places WHERE id = ?`, [], function(err) {
+          if(err) {
+              return console.log(err)
+          }
 
-    //      console.log("Registro deletado com sucesso!")
-    //  })
-//})
+          console.log("Registro deletado com sucesso!")
+      }) */
--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ const db = require("./database/db")
 // configurar pasta publica
 server.use(express.static("public"))
 
+// habilitar o uso do req.body na nossa aplicação
+    server.use(express.urlencoded({ extended: true}))
+
+
 
 // Utilizando template Engine
 const nunjucks = require("nunjucks")
@@ -24,25 +28,85 @@ server.get("/", (req, res) => {
     return res.render("index.html", { title: "Um título"})
 })
 
+
+
+
 server.get("/create-point", (req, res) => {
+
+
+
+
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+     
+    
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items   
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro no Cadastro")
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData) 
+  
+  
+    
+})
+
+
 server.get("/search", (req, res) => {
+
+const search = req.query.search 
+
+if(search == "") {
+    return res.render("search-results.html", {total: 0 })
+
+}
 
 
 /* Pegar os dados do banco de dados */
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
                 if(err) {
                     return console.log(err)
             }
+
+            const total = rows.length
         
             console.log("Aqui estão seus registros")
             console.log(rows)
+
+            //Mostrar a página html com os dados do banco de dados
+            return res.render("search-results.html", {places: rows, total })
             })
-
-
-    return res.render("search-results.html")
 })
 
 
